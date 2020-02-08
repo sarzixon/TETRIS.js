81188fa089d0a0742bf97aea865195e8a7532483
Michał Sarzała <sarzal666@gmail.com>
2020-02-08 19:39:22 +0100

    sketch is done

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,29 @@
 import '../style.css';
+
+const canvas = document.querySelector('#game');
+const ctx = canvas.getContext('2d');
+
+
+// create a net of blocks
+const canvasWidth = 300;
+const canvasHeight = 600;
+const columns = 10;
+const rows = 20;
+const blockSize = 30;
+// console.log(blockSize);
+ctx.strokeStyle = "#a19494";
+for(let i = 30; i < canvasHeight; i+=blockSize) {
+    ctx.beginPath();
+    ctx.moveTo(i,0);
+    ctx.lineTo(i, canvasHeight);
+    ctx.moveTo(0,i);
+    ctx.lineTo(canvasWidth, i);
+    ctx.stroke();
+    // console.log(i);
+};
+
+
+// one block
+
+// ctx.rect(135,100,blockSize,blockSize);
+// ctx.stroke();
